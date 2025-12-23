6c205653ea77e3a56f69416a8eab78bbd182ef72
Aditya-Kompella <47743467+Aditya-Kompella@users.noreply.github.com>
2025-12-23 14:20:08 +0530

    3CountNumber.js added recursion function

--- 3CountNumber.js
+++ 3CountNumber.js
@@ -9,4 +9,20 @@ function countNumbers(n) {
     }
     return count;
 }
-console.log(countNumbers(-55555555555555555));
+// console.log(countNumbers(-55555555555555555));
+function countNumbersRecursion(n){
+  n = Math.abs(n);
+  if(n===0) return 1;
+  count = 0;
+  function helper(x){
+    if(x===0) return 0;
+    return 1+helper(Math.floor(x/10));
+  }
+  return helper(n);
+}
+console.log(countNumbersRecursion(5555))
+console.log(countNumbersRecursion(-5555))
+console.log(countNumbersRecursion(2))
+console.log(countNumbersRecursion(0))
+console.log(countNumbersRecursion(1234567890))
+
